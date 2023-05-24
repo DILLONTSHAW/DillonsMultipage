03a4ad6c5216dacede1e4b803de516e78face1f1
DILLONTSHAW <124849112+DILLONTSHAW@users.noreply.github.com>
2023-05-24 10:19:12 -0400

    Add files via upload

---
+++ DillonsMultipage/Webpage/script-page1.js
@@ -0,0 +1,4 @@
+// JavaScript for Page 1
+
+// Add any custom JavaScript code specific to Page 1
+console.log('Page 1 JavaScript');
---
+++ DillonsMultipage/Webpage/script-page3.js
@@ -0,0 +1,4 @@
+// JavaScript for Page 3
+
+// Add any custom JavaScript code specific to Page 3
+console.log('Page 3 JavaScript');
